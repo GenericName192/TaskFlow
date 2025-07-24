ae69354f5acf879c776290edc6360f07f3cc7520
GenericName192 <David-barton192@hotmail.co.uk>
2025-07-24 15:29:52 +0100

    set up urls.py inside the authuser app and added the __str__ function to the model

--- authuser/models.py
+++ authuser/models.py
@@ -7,3 +7,6 @@ class User(AbstractUser):
     boss = models.ForeignKey(
         'self', on_delete=models.SET_NULL, null=True, blank=True,
         related_name='subordinates', verbose_name='Boss')
+
+    def __str__(self):
+        return self.username
