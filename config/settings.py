1f5e3b9a5fbc31573027017b3b2f34f78a2864e6
GenericName192 <David-barton192@hotmail.co.uk>
2025-07-24 13:49:53 +0100

    Created both apps that I will need for the project: authuser and task

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "authuser",  # Custom user app
+    "task",  # Task management app
 ]
 
 MIDDLEWARE = [
