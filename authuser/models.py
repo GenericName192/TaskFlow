67f4709e887e713791b43b6eeab9d1fbf2289b08
GenericName192 <David-barton192@hotmail.co.uk>
2025-07-25 13:53:44 +0100

    added function get_direct_subordinates and get_all_subordinates to allow for querying the User table

--- authuser/models.py
+++ authuser/models.py
@@ -6,7 +6,25 @@ class User(AbstractUser):
     """Custom user model extending Django's AbstractUser."""
     boss = models.ForeignKey(
         'self', on_delete=models.SET_NULL, null=True, blank=True,
-        related_name='subordinates', verbose_name='Boss')
+        related_name='subordinates', verbose_name='Boss',
+        default="No boss assigned")
 
     def __str__(self):
         return self.username
+
+    def get_direct_subordinates(self):
+        """Return a queryset of direct subordinates."""
+        return self.subordinates.all()
+
+    def get_all_subordinates(self, collection=None):
+        """Return a set of all subordinates, including indirect ones."""
+        if collection is None:
+            collection = set()
+
+        direct_subordinates = self.get_direct_subordinates()
+
+        for subordinate in direct_subordinates:
+            if subordinate not in collection:
+                collection.add(subordinate)
+                subordinate.get_all_subordinates(collection)
+        return collection
