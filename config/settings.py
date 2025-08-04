a3549b054a8b2bf114a66378bc035e9bb6f8818b
GenericName192 <David-barton192@hotmail.co.uk>
2025-08-04 13:52:36 +0100

    fixed a bug in base.html and added some whitenoise config for debugging

--- config/settings.py
+++ config/settings.py
@@ -138,12 +138,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # WhiteNoise configuration
 STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
+
+# Simplified static files serving for debugging
+WHITENOISE_USE_FINDERS = True
+WHITENOISE_AUTOREFRESH = True
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
