729fc9ddf2b77966c6c34b7c408a06a196e185cb
GenericName192 <David-barton192@hotmail.co.uk>
2025-08-12 11:21:15 +0100

    Updated admin so that the hashed passwords are no longer visable on the admin panel.

--- authuser/admin.py
+++ authuser/admin.py
@@ -2,9 +2,29 @@ from django.contrib import admin
 from django.contrib.auth.models import Group
 from django.contrib.sites.models import Site
 from .models import User
+from django.contrib.auth.admin import UserAdmin
 
 
-admin.site.register(User)
+class authUser(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('username', 'email')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'boss')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff')}),
+        ('Important dates', {'fields': ('date_joined',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'first_name', 'last_name',
+                       'boss', 'password1', 'password2'),
+        }),
+    )
+
+    readonly_fields = ('date_joined',)
+
+
+admin.site.register(User, authUser)
 
 
 # unregister the unused models
